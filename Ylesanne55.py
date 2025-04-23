d1b399a95446152585afc759929529b0ab23d66e
Eiko Evel <eiko.evel@vikk.ee>
2025-04-23 20:11:16 +0300

    a

--- Ylesanne55.py
+++ Ylesanne55.py
@@ -9,59 +9,63 @@ pink = [255, 153, 255]
 lGreen = [153, 255, 153]
 lBlue = [153, 204, 255]
 
-# ekraani seaded
+#ekraani seaded
 screenX = 640
 screenY = 480
-screen = pygame.display.set_mode([screenX, screenY])
+screen=pygame.display.set_mode([screenX,screenY])
 pygame.display.set_caption("Ping-Pong")
+screen.fill(lBlue)
 clock = pygame.time.Clock()
-
-# algseisud
+posX, posY = 0, 0
 ballspeedX, ballspeedY = 3, 4
-padspeed = 3
+padspeed = 4
 
-# ball
-ball = pygame.Rect(100, 100, 20, 20)
+#ball
+ball = pygame.Rect(posX, posY, 20, 20)
 ballImage = pygame.image.load("ball.png")
 ballImage = pygame.transform.scale(ballImage, [ball.width, ball.height])
 
-# pad (aseta see allapoole ekraani keskele)
-pad = pygame.Rect(100, int(screenY * 0.75), 120, 20)
+#pad
+pad = pygame.Rect(posX, posY, 120, 20)
 padImage = pygame.image.load("pad.png")
 padImage = pygame.transform.scale(padImage, [pad.width, pad.height])
 
-# game loop
+score = 0
+
 gameover = False
 while not gameover:
     clock.tick(60)
     screen.fill(lBlue)
-
-    # sündmused
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameover = True
+    #mängu sulgemine ristist
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT:
+        break
 
     # ball liikumine
-    ball.x += ballspeedX
-    ball.y += ballspeedY
+    ball = pygame.Rect(posX, posY, 20, 20)
+    screen.blit(ballImage, ball)
 
-    # seinte põrkamine
-    if ball.left <= 0 or ball.right >= screenX:
+    posX += ballspeedX
+    posY += ballspeedY
+
+    if posX > screenX - ballImage.get_rect().width or posX < 0:
         ballspeedX = -ballspeedX
-    if ball.top <= 0:
+
+    if posY > screenY - ballImage.get_rect().height or posY < 0:
         ballspeedY = -ballspeedY
 
     # pad liikumine
     pad.x += padspeed
+    pad.y = screenY * 0.75
     if pad.left <= 0 or pad.right >= screenX:
         padspeed = -padspeed
 
-    # põrkumine padiga (ainult kui liigub alla)
     if ball.colliderect(pad) and ballspeedY > 0:
         ballspeedY = -ballspeedY
+        score += 1
+        print(score) # lisab skoori ühe punkti võrra ning väljastab selle terminali
 
-    # joonista
-    screen.blit(ballImage, ball)
     screen.blit(padImage, pad)
-
     pygame.display.flip()
+
+
